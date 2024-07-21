ed49ba0bcf303ebc8f9aad114dd179b8372d0c27
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-07-22 01:55:01 +0530

    added our solutions page

--- src/components/home/OurSolutions.js
+++ src/components/home/OurSolutions.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import styles from "../../styles/Home.module.scss";
+
 
 const OurSolutions = () => {
   return (
-    <div>OurSolutions</div>
+    <div className={styles.our_solutions}>
+      <h3>
+        The Art of <div><span>Strategic</span></div> <br /> Creativity
+      </h3>
+      <p>Creative Studio follows a collaborative and iterative approach to creation, with a <br />
+        focus on understanding and meeting the unique needs of each client.</p>
+    </div>
   )
 }
 
