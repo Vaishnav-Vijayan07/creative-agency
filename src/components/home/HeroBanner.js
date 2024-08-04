22f11808830256a74b38cb4f9a45bdea01b3e219
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-08-04 21:12:42 +0530

    video player issue fixed

--- src/components/home/HeroBanner.js
+++ src/components/home/HeroBanner.js
@@ -6,7 +6,7 @@ const HeroBanner = () => {
   return (
     <div style={{ height: "100%" }}>
       <div className={styles.video_container}>
-        <video autoPlay muted loop  playsInline className={styles.video} id="background-video">
+        <video muted autoPlay loop playsInline preload="metadata" aria-label="Video player" className={styles.video} id="background-video">
           <source src="/video/bg_video.mp4" type="video/mp4" />
         </video>
       </div>
