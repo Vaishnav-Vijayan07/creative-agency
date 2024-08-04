6a55b360059d24add6fd34157b8f1c073c769d49
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-08-05 00:03:15 +0530

    added poster

--- src/components/home/HeroBanner.js
+++ src/components/home/HeroBanner.js
@@ -6,7 +6,7 @@ const HeroBanner = () => {
   return (
     <div style={{ height: "100%" }}>
       <div className={styles.video_container}>
-        <video muted loop playsInline preload="metadata" aria-label="Video player" className={styles.video} id="background-video">
+        <video muted autoPlay loop playsInline preload="metadata" poster="/images/background.png" aria-label="Video player" className={styles.video} id="background-video">
           <source src="/video/bg_video.mp4" type="video/mp4" />
           This video is not supported in this browser
         </video>
